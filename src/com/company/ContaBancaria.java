f4bf01298200270adcd1709c549b4047436e19a2
JessicaQueiroz <jehqueiroz01@gmail.com>
2021-01-24 10:32:15 -0300

    Initial commit

---
+++ src/com/company/ContaBancaria.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ContaBancaria {
+
+    private int numeroConta;
+    private double saldo;
+
+    public ContaBancaria(int nConta){
+        System.out.println("Nùmero da conta: " + nConta);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria(100057896);
+    }
+}
